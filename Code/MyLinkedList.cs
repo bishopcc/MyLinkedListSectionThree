8de277fc43b87da4a4e8e359d001850dce16e99a
bishopcc <bishopcc@rose-hulman.edu>
2016-03-15 12:53:15 -0700

    All of Live Coding

---
+++ Code/MyLinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    public class MyLinkedList
+    {
+        int adds = 0;
+        public void add(int v)
+        {
+            adds = adds + 1;
+        }
+
+        public int size()
+        {
+            return adds;
+        }
+
+        public bool contains(int v)
+        {
+            return adds > 0;
+        }
+
+        public bool hasNext()
+        {
+            return adds > 0;
+        }
+
+        public int? getNext()
+        {
+            if (adds == 0)
+            {
+                return null;
+            } else
+            {
+                return 1;
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Code;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            MyLinkedList list = new MyLinkedList();
+            list.add(0);
+            Assert.AreEqual(1, list.size());
+
+        }
+
+        [TestMethod]
+        public void TestContains()
+        {
+            MyLinkedList list = new MyLinkedList();
+            list.add(0);
+            Assert.IsTrue(list.contains(0));
+        }
+
+        [TestMethod]
+        public void TestSize()
+        {
+            MyLinkedList list = new MyLinkedList();
+            list.add(0);
+            Assert.AreEqual(1, list.size());
+            list.add(1);
+            Assert.AreEqual(2, list.size());
+        }
+        [TestMethod]
+        public void TestNotContains()
+        {
+            MyLinkedList list = new MyLinkedList();
+            Assert.IsFalse(list.contains(0));
+        }
+
+        [TestMethod]
+        public void TestNext()
+        {
+            MyLinkedList list = new MyLinkedList();
+            Assert.IsFalse(list.hasNext());
+            Assert.IsNull(list.getNext());
+            list.add(1);
+            Assert.IsTrue(list.hasNext());
+            Assert.IsNotNull(list.getNext());
+        }
+    }
+}
